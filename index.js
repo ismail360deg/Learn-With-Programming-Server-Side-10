372bfa21a2286fa656a9068dba9f96244ee52e2a
ismail360deg <ismailbhuiyan2000@gmail.com>
2022-10-27 01:34:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+app.get('/', (req, res) => {
+    res.send('News API Running');
+});
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+});
+
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCode = categories.find(n => n.id == id);
+    res.send(selectedCode);
+});
+
+app.listen(port, () => {
+    console.log('Dragon News Server running on port', port);
+})
+
+module.exports = app;
